b3f94a72e18b55e6554991c6d4fd34aa7f22d8f7
Pasang-lama <lpasang182@gmail.com>
2024-09-16 08:29:26 +0545

    wroks on the slider and offer section

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:8,
+        loop:true,
+        nav: false,
+        // navText: [ '<i class="fa-solid fa-left-long"></i>', '<i class="fa-solid fa-right-long"></i>']
+        dots: false
+    });
+  });
